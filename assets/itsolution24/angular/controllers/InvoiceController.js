dfeb97e2a98f5e0476d3e9a5135d9d6e849a063c
Geoffreey Cali <geoffrey.torres64@gmail.com>
2024-05-12 14:54:51 -0600

    suma de total de columa 6

--- assets/itsolution24/angular/controllers/InvoiceController.js
+++ assets/itsolution24/angular/controllers/InvoiceController.js
@@ -180,6 +180,17 @@ window.angularApp.controller("InvoiceController", [
             ? i
             : 0;
         };
+        // Total over all pages at column 6
+        pageTotal = api
+        .column( 6, { page: "current"} )
+        .data()
+        .reduce( function (a, b) {
+            return intVal(a) + intVal(b);
+        }, 0 );
+    // Actualizar pie de página
+    $( api.column( 6 ).footer() ).html(
+        window.formatDecimal(pageTotal, 2)
+    );
       },
       pageLength: window.settings.datatable_item_limit,
       buttons: [
